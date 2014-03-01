266203d1ca49171c87039e3967f0614569e7eb99
KJ Prince <kenprincejr@gmail.com>
2014-02-28 19:46:18 -0500

    added caching

--- app/local-config.php
+++ app/local-config.php
@@ -1,24 +1,37 @@
 <?php
 /*
 This is a sample local-config.php file
+
 In it, you *must* include the four main database defines
 
 You may include other settings here that you only want enabled on your local development checkouts
+
+This file should be ignored in your .gitignore file
 */
 
-define( 'DB_NAME', 'wordpress' );
-define( 'DB_USER', 'wp' );
-define( 'DB_PASSWORD', 'wp' );
-define( 'DB_HOST', '127.0.0.1' ); // Probably 'localhost'
+define( 'DB_NAME', 'wp013' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'localhost:/var/run/mysqld/mysqld.sock' ); // Probably 'localhost' I'm using sockets
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
+// ========================
+// Custom Content Directory
+// ========================
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
-// ==================
-//SETUP MULTISITE? 
-// UNCOMMENT BELOW
-// ==================
-//define('WP_ALLOW_MULTISITE', true);
+// ==============================================================
+// Table prefix
+// Change this if you have multiple installs in the same database
+// ==============================================================
+$table_prefix  = 'rs1_';
+
+// ======================
+// ENABLE MULTISITE? 
+// ======================
+// define('WP_ALLOW_MULTISITE', true);
 
 // ====================
 // SETUP MULTISITE 
@@ -26,75 +39,68 @@ define('DB_COLLATE', '');
 // define('MULTISITE', true);
 // define('SUBDOMAIN_INSTALL', true);
 // $base = '/';
-// define('DOMAIN_CURRENT_SITE', 'mu.rtcamp.net');
+// define('DOMAIN_CURRENT_SITE', 'wordpress.dev');
 // define('PATH_CURRENT_SITE', '/');
 // define('SITE_ID_CURRENT_SITE', 1);
 // define('BLOG_ID_CURRENT_SITE', 1);
 
+// ================================
+// Enable CACHING - Batcache / APC
+//=================================
+define( 'WP_CACHE', true );
 
-// ===================
+// ====================================
+// ENABLE MULTI SITE DOMAIN MAPPING
+// ====================================
+// define( 'SUNRISE', 'on' );
+
+// =======================
 // Define Memory Limit
-// ===================
+// =======================
 define('WP_MEMORY_LIMIT', '128M');
 define('WP_MAX_MEMORY_LIMIT', '128M');
 
-// ===========================
-// Enable APC / Batcache
-//
-define( 'WP_CACHE', true );
-// 
-//=============================
-
 // =====================
 // EMPTY THE TRASH 
-//
+// =====================
 // define( 'EMPTY_TRASH_DAYS', 30 ); // 30 days
-//
-// =====================
 
-// =====================
-// Auto Optimize Database
-//
-// define( 'WP_ALLOW_REPAIR', true );
-//
 // =======================
+// Auto Optimize Database
+// =======================
+// define( 'WP_ALLOW_REPAIR', true );
 
 // =======================================================
-// Define site and blog URLs
-//
-// define( 'WP_SITEURL', 'http://example.com/wp' );
-//
-// define( 'WP_HOME', 'http://example.com' );
-//
+// DEFINE SITE AND BLOG URLS - IMPORTANT!
 // =======================================================
+define( 'WP_SITEURL', 'http://wordpress.dev/wp' );
+define( 'WP_HOME', 'http://wordpress.dev' );
 
-// =====================
-// Disable post revisions
+// ==========================
+// DISABLE POST REVISIONS
+// ==========================
 // define( 'WP_POST_REVISIONS', false );
-// ======================
 
-// ==============================
-// Force SSl for Admin and Login
-// define( 'FORCE_SSL_LOGIN', true );
 // ==================================
+// FORCE SSL FOR ADMIN AND LOGIN
+// ==================================
+// define( 'FORCE_SSL_LOGIN', true );
 
-// ====================================================
-// Disable Plugin and Theme Updating and Installation
-//
+// ================================================
+// DISABLE PLUGIN AND THEME UPDATING AND INSTALL
+// ================================================
 // define( 'DISALLOW_FILE_MODS', true );
-//
-// ====================================================
 
 // ====================================
 // Disallow Editing From Dashboard
 // Disables Plugin and Theme Editor
-//
+// ====================================
 // define('DISALLOW_FILE_EDIT', true);
-//
-// ===================
 
-// ==============================================================
-// Table prefix
-// Change this if you have multiple installs in the same database
-// ==============================================================
-//$table_prefix  = 'rs1_';
+// ======================
+// BOOTSTRAP WORDPRESS 
+// ======================
+if ( !defined( 'ABSPATH' ) )
+define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+require_once( ABSPATH . 'wp-settings.php' );
+// =====================
